ecae2b3c6cffe1455ba8948e4db07457136db57e
Taylor Behrens <126136711+taylorbeee@users.noreply.github.com>
2023-05-22 20:24:44 -0700

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,22 @@
+var box1 = document.querySelector(".box-1");
+var box2 = document.querySelector(".box-2");
+var box3 = document.querySelector(".box-3");
+
+document.addEventListener("keydown", function (e) {
+  var key = e.key;
+  if (key === "1") {
+    box1.innerText = "🔥";
+  } else if (key === "2") {
+    box2.innerText = "💀";
+  } else if (key === "3") {
+    box3.innerText = "🚀";
+  }
+});
+
+var button = document.querySelector("button");
+
+button.addEventListener("click", function () {
+  box1.innerText = "1";
+  box2.innerText = "2";
+  box3.innerText = "3";
+});
